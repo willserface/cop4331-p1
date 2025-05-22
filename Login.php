b84a1f521e800cf9752ffc35fe98da3f1e94d5f8
mkelly674 <114030563+mkelly674@users.noreply.github.com>
2025-05-22 13:05:49 -0400

    Update Login.php

--- Login.php
+++ Login.php
@@ -7,7 +7,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "shark-paradise"); 	
+	$conn = new mysqli("localhost", "TheBeast", "COP4331COP", "shark-paradise"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
