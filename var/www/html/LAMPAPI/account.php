5ebb36670d24ab21d1115a213cfa39eeb06fd819
willserface <will@serface.com>
2025-05-24 22:32:02 -0400

    Combines `addAccount`, `Login`, `editContact` and `deleteContact` into a single PHP file

---
+++ var/www/html/LAMPAPI/account.php
@@ -0,0 +1,171 @@
+<?php
+$inData = getRequestInfo();
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+
+$username = $_SERVER['PHP_AUTH_USER'];
+$password = $_SERVER['PHP_AUTH_PW'];
+
+$firstName = $inData["name"]["first"];
+$lastName = $inData["name"]["last"];
+
+$conn = new mysqli("localhost", "Swimmer", "Swim1", "COP4331");
+
+if ($conn->connect_error) {
+    returnWithError("Failed to connect to MySQL");
+} else switch ($method) {
+    case 'POST':
+        postAccount($conn);
+        break;
+    case 'GET':
+        getAccount($conn);
+        break;
+    case 'PUT':
+        putAccount($conn);
+        break;
+    case 'DELETE':
+        deleteAccount($conn);
+        break;
+}
+
+$conn->close();
+
+function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj) {
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err) {
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo( $username, $firstName, $lastName )
+{
+    $retValue = '{"username":' . $username . ',"name": {"first": ' . $firstName . ',"last": ' . $lastName . '},"error":null"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+
+function postAccount($conn) {
+    global $inData;
+    global $firstName;
+    global $lastName;
+
+    $username = $inData["username"];
+    $password = $inData["password"];
+
+    $checkForExisting = $conn->prepare("SELECT * FROM Users WHERE Login = ?");
+    $checkForExisting->bind_param("s", $username);
+    $checkForExisting->execute();
+
+    if ($checkForExisting->num_rows == 0) {
+        $insert = $conn->prepare("INSERT INTO Users (Login, FirstName, LastName, Password) VALUES (?, ?, ?, ?)");
+        $insert->bind_param("ssss", $username, $firstName, $lastName, $password);
+        $insert->execute();
+        $insert->close();
+        returnWithError("201");
+    } else {
+        returnWithError("400");
+    }
+
+    $checkForExisting->close();
+}
+
+function authenticated() {
+    global $conn;
+
+    $auth = $conn->prepare("SELECT * FROM Users WHERE Login = ? AND Password = ?");
+    $auth->bind_param("ss", $username, $password);
+    $auth->execute();
+    $result = $auth->get_result();
+
+    return $result->num_rows == 1;
+}
+
+function getAccount($conn) {
+    global $username;
+    global $password;
+
+    $login = $conn->prepare("SELECT * FROM Users WHERE Login = ? AND Password = ?");
+    $login->bind_param("ss", $username, $password);
+    $login->execute();
+    $result = $login->get_result();
+
+    if ($row = $result->fetch_assoc()) {
+        returnWithInfo($row["Login"], $row["FirstName"], $row["LastName"]);
+    } else {
+        returnWithError("404");
+    }
+
+    $login->close();
+}
+
+function putAccount($conn)
+{
+    global $inData;
+    global $username;
+    global $firstName;
+    global $lastName;
+
+    $newUsername = $inData["username"];
+    $newPassword = $inData["password"];
+
+    if (authenticated()) {
+
+        if ($newUsername != null) {
+            $updatePrimaryKey = $conn->prepare("UPDATE Users SET Login = ? WHERE Login = ?");
+            $updatePrimaryKey->bind_param("ss", $newUsername, $username);
+            $updatePrimaryKey->execute();
+            $updatePrimaryKey->close();
+
+            $updateForeignKey = $conn->prepare("UPDATE Contacts SET UserID = ? WHERE UserID = ?");
+            $updateForeignKey->bind_param("ss", $newUsername, $username);
+            $updateForeignKey->execute();
+            $updateForeignKey->close();
+        }
+
+        if ($newPassword != null) {
+            $updatePassword = $conn->prepare("UPDATE Users SET Password = ? WHERE Login = ?");
+            $updatePassword->bind_param("ss", $newPassword, $username);
+            $updatePassword->execute();
+            $updatePassword->close();
+        }
+
+        if ($firstName != null) {
+            $updateFirstName = $conn->prepare("UPDATE Users SET FirstName = ? WHERE Login = ?");
+            $updateFirstName->bind_param("ss", $firstName, $username);
+            $updateFirstName->execute();
+            $updateFirstName->close();
+        }
+
+        if ($lastName != null) {
+            $updateLastName = $conn->prepare("UPDATE Users SET LastName = ? WHERE Login = ?");
+            $updateLastName->bind_param("ss", $lastName, $username);
+            $updateLastName->execute();
+            $updateLastName->close();
+        }
+
+        returnWithError("201");
+
+    } else {
+        returnWithError("401");
+    }
+}
+
+function deleteAccount($conn)
+{
+    global $username;
+
+    if (authenticated()) {
+        $delete = $conn->prepare("DELETE FROM Users WHERE Login = ?");
+        $delete->bind_param("s", $username);
+        $delete->execute();
+        $delete->close();
+    }
+}
